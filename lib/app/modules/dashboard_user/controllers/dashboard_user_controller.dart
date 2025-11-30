17e836c2a1425d43c77d2b46a27df9940e7b9367
amma1104 <ammahikam11@gmail.com>
2025-11-30 21:31:10 +0700

    Amma - update tampilan user dan navigasi bar

--- lib/app/modules/dashboard_user/controllers/dashboard_user_controller.dart
+++ lib/app/modules/dashboard_user/controllers/dashboard_user_controller.dart
@@ -4,7 +4,17 @@ import 'package:get/get.dart';
 class DashboardUserController extends GetxController {
   final _authRepo = AuthenticationRepository.instance;
 
+  final userName = 'Halo Fufufafa!'.obs;
+  final currentDate = 'Selasa, 11 November 2025'.obs;
+  final currentTime = '11:00'.obs;
+
+  final selectedIndex = 0.obs;
+
   void logout() {
     _authRepo.logout();
   }
+  void changeTab(int index) {
+    selectedIndex.value = index;
+  }
 }
+
