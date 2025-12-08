49e0b5ef16ea29b6b63c19cf8336f142e56a2c4f
Mada29 <madarabbanisyah@webmail.umm.ac.id>
2025-12-09 00:34:49 +0700

    Mada

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,28 +1,36 @@
 // ignore_for_file: constant_identifier_names
 
+import 'package:get/get.dart';
+
+// AUTH
 import 'package:ecocampus/app/modules/auth/bindings/auth_binding.dart';
 import 'package:ecocampus/app/modules/auth/bindings/reset_password_binding.dart';
-import 'package:ecocampus/app/modules/auth/views/forgot_password_view.dart';
-import 'package:ecocampus/app/modules/auth/views/reset_password_view.dart';
-import 'package:ecocampus/app/modules/dashboard_admin/views/activity/activity_admin_view.dart';
-import 'package:ecocampus/app/modules/dashboard_admin/views/activity/activity_list_view.dart';
-import 'package:get/get.dart';
 import 'package:ecocampus/app/modules/auth/views/login_view.dart';
 import 'package:ecocampus/app/modules/auth/views/register_view.dart';
-import 'package:ecocampus/app/modules/dashboard_admin/views/dashboard_admin_view.dart';
-import 'package:ecocampus/app/modules/dashboard_admin/bindings/dashboard_admin_binding.dart';
-import 'package:ecocampus/app/modules/dashboard_user/views/dashboard_user_view.dart';
+import 'package:ecocampus/app/modules/auth/views/forgot_password_view.dart';
+import 'package:ecocampus/app/modules/auth/views/reset_password_view.dart';
+
+// USER
 import 'package:ecocampus/app/modules/dashboard_user/bindings/dashboard_user_binding.dart';
+import 'package:ecocampus/app/modules/dashboard_user/views/dashboard_user_view.dart';
+import 'package:ecocampus/app/modules/dashboard_user/page_kolaborasi_kampus/bindings/kolaborasi_kampus_binding.dart';
+import 'package:ecocampus/app/modules/dashboard_user/page_kolaborasi_kampus/views/kolaborasi_kampus_view.dart';
+
+// ADMIN
+import 'package:ecocampus/app/modules/dashboard_admin/bindings/dashboard_admin_binding.dart';
+import 'package:ecocampus/app/modules/dashboard_admin/views/dashboard_admin_view.dart';
+import 'package:ecocampus/app/modules/dashboard_admin/views/activity/activity_admin_view.dart';
+import 'package:ecocampus/app/modules/dashboard_admin/views/activity/activity_list_view.dart';
 
 part 'app_routes.dart';
 
 class AppPages {
   AppPages._();
-  
-  //auth
+
   static const INITIAL = Routes.LOGIN;
 
   static final routes = <GetPage>[
+    // ---------------- AUTH ----------------
     GetPage(
       name: _Paths.LOGIN,
       page: () => const LoginView(),
@@ -44,14 +52,20 @@ class AppPages {
       binding: ResetPasswordBinding(),
     ),
 
-    //user
+    // ---------------- USER ----------------
     GetPage(
       name: _Paths.DASHBOARD_USER,
       page: () => const DashboardUserView(),
       binding: DashboardUserBinding(),
     ),
 
-    //admin
+    GetPage(
+      name: _Paths.KOLABORASI_KAMPUS,
+      page: () => const KolaborasiKampusView(),
+      binding: KolaborasiKampusBinding(),
+    ),
+
+    // ---------------- ADMIN ----------------
     GetPage(
       name: _Paths.DASHBOARD_ADMIN,
       page: () => const DashboardAdminView(),
--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -5,17 +5,17 @@ part of 'app_pages.dart';
 abstract class Routes {
   Routes._();
 
-  //auth
+  // AUTH
   static const LOGIN = _Paths.LOGIN;
   static const REGISTER = _Paths.REGISTER;
   static const FORGOT_PASSWORD = _Paths.FORGOT_PASSWORD;
   static const RESET_PASSWORD = _Paths.RESET_PASSWORD;
 
-  //user
+  // USER
   static const DASHBOARD_USER = _Paths.DASHBOARD_USER;
+  static const KOLABORASI_KAMPUS = _Paths.KOLABORASI_KAMPUS;
 
-
-  //admin
+  // ADMIN
   static const DASHBOARD_ADMIN = _Paths.DASHBOARD_ADMIN;
   static const ADMIN_ACTIVITY = _Paths.ADMIN_ACTIVITY;
   static const ADMIN_ACTIVITY_LIST = _Paths.ADMIN_ACTIVITY_LIST;
@@ -25,15 +25,17 @@ abstract class Routes {
 abstract class _Paths {
   _Paths._();
 
+  // AUTH
   static const LOGIN = '/login';
   static const REGISTER = '/register';
   static const FORGOT_PASSWORD = '/forgot-password';
   static const RESET_PASSWORD = '/reset-password';
 
-  //user
+  // USER
   static const DASHBOARD_USER = '/dashboard-user';
+  static const KOLABORASI_KAMPUS = '/kolaborasi-kampus';
 
-  //admin
+  // ADMIN
   static const DASHBOARD_ADMIN = '/dashboard-admin';
   static const ADMIN_ACTIVITY = '/admin-activity';
   static const ADMIN_ACTIVITY_LIST = '/admin-activity-list';
