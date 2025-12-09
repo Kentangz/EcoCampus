825c1040234b8352766171e543e0a41ebf4c0314
Kentangz <adhitsholawat12@gmail.com>
2025-12-09 09:25:26 +0700

    fix previous code structure

--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -5,17 +5,17 @@ part of 'app_pages.dart';
 abstract class Routes {
   Routes._();
 
-  // AUTH
+  //auth
   static const LOGIN = _Paths.LOGIN;
   static const REGISTER = _Paths.REGISTER;
   static const FORGOT_PASSWORD = _Paths.FORGOT_PASSWORD;
   static const RESET_PASSWORD = _Paths.RESET_PASSWORD;
 
-  // USER
+  //user
   static const DASHBOARD_USER = _Paths.DASHBOARD_USER;
   static const KOLABORASI_KAMPUS = _Paths.KOLABORASI_KAMPUS;
 
-  // ADMIN
+  //admin
   static const DASHBOARD_ADMIN = _Paths.DASHBOARD_ADMIN;
   static const ADMIN_ACTIVITY = _Paths.ADMIN_ACTIVITY;
   static const ADMIN_ACTIVITY_LIST = _Paths.ADMIN_ACTIVITY_LIST;
@@ -25,17 +25,17 @@ abstract class Routes {
 abstract class _Paths {
   _Paths._();
 
-  // AUTH
+  //auth
   static const LOGIN = '/login';
   static const REGISTER = '/register';
   static const FORGOT_PASSWORD = '/forgot-password';
   static const RESET_PASSWORD = '/reset-password';
 
-  // USER
+  //user
   static const DASHBOARD_USER = '/dashboard-user';
   static const KOLABORASI_KAMPUS = '/kolaborasi-kampus';
 
-  // ADMIN
+  //admin
   static const DASHBOARD_ADMIN = '/dashboard-admin';
   static const ADMIN_ACTIVITY = '/admin-activity';
   static const ADMIN_ACTIVITY_LIST = '/admin-activity-list';
