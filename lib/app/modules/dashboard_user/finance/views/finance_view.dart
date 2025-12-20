f0bbb5286b4966aa89e14fe380af462b353990a2
iswhyd <iswahyudiiskandar@gmail.com>
2025-12-20 13:56:34 +0700

    update finance_view

--- lib/app/modules/dashboard_user/finance/views/finance_view.dart
+++ lib/app/modules/dashboard_user/finance/views/finance_view.dart
@@ -1,7 +1,44 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
-class FinanceView extends StatelessWidget {
+class FinanceView extends StatefulWidget {
   const FinanceView({super.key});
+
+  @override
+  State<FinanceView> createState() => _FinanceViewState();
+}
+
+class _FinanceViewState extends State<FinanceView> {
+  List articles = [];
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchNews();
+  }
+
+  Future<void> fetchNews() async {
+    final url = Uri.parse(
+      "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=c0a4ffdc12da4de18e879c1ccbcf649e",
+    );
+
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      setState(() {
+        articles = data['articles'];
+        isLoading = false;
+      });
+    } else {
+      setState(() {
+        isLoading = false;
+      });
+      throw Exception("Gagal mengambil berita");
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,32 +66,39 @@ class FinanceView extends StatelessWidget {
           ),
           const SizedBox(height: 20),
 
-          GridView.builder(
-            itemCount: 6,
-            shrinkWrap: true,
-            physics: const NeverScrollableScrollPhysics(),
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              crossAxisSpacing: 16,
-              mainAxisSpacing: 16,
-              childAspectRatio: 0.85,
-            ),
-            itemBuilder: (context, index) {
-              return financeCard();
-            },
-          ),
+          isLoading
+              ? const Center(child: CircularProgressIndicator())
+              : GridView.builder(
+                  itemCount: articles.length,
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  gridDelegate:
+                      const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 16,
+                    mainAxisSpacing: 16,
+                    childAspectRatio: 0.85,
+                  ),
+                  itemBuilder: (context, index) {
+                    final article = articles[index];
+                    return financeCard(
+                      title: article['title'] ?? "No Title",
+                      imageUrl: article['urlToImage'],
+                    );
+                  },
+                ),
         ],
       ),
     );
   }
 
-  Widget financeCard() {
+  Widget financeCard({
+    required String title,
+    String? imageUrl,
+  }) {
     return Container(
       decoration: BoxDecoration(
-        border: Border.all(
-          color: Colors.black,
-          width: 1,
-        ),
+        border: Border.all(color: Colors.black),
         color: Colors.white,
         borderRadius: BorderRadius.circular(16),
         boxShadow: [
@@ -69,24 +113,33 @@ class FinanceView extends StatelessWidget {
         children: [
           ClipRRect(
             borderRadius: const BorderRadius.vertical(top: Radius.circular(16)),
-            child: Image.network(
-              "https://i.ibb.co/6HkG1v1/finance.jpg",
-              height: 100,
-              width: double.infinity,
-              fit: BoxFit.cover,
-            ),
+            child: imageUrl != null
+                ? Image.network(
+                    imageUrl,
+                    height: 100,
+                    width: double.infinity,
+                    fit: BoxFit.cover,
+                  )
+                : Container(
+                    height: 100,
+                    color: Colors.grey[300],
+                    child: const Icon(Icons.image_not_supported),
+                  ),
           ),
           Container(
             width: double.infinity,
             padding: const EdgeInsets.all(12),
             decoration: const BoxDecoration(
               color: Color(0xffCDEEFF),
-              borderRadius: BorderRadius.vertical(bottom: Radius.circular(16)),
+              borderRadius:
+                  BorderRadius.vertical(bottom: Radius.circular(16)),
             ),
-            child: const Text(
-              "Berita Keuangan",
-              style: TextStyle(
-                fontSize: 16,
+            child: Text(
+              title,
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+              style: const TextStyle(
+                fontSize: 14,
                 fontWeight: FontWeight.w600,
               ),
             ),
