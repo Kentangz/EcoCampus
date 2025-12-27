719e564fe5c8c3773eba2ddbb88f25019be5d219
iswhyd <iswahyudiiskandar@gmail.com>
2025-12-28 03:41:52 +0700

    update-front-end-finance

--- lib/app/modules/dashboard_user/finance/views/finance_view.dart
+++ lib/app/modules/dashboard_user/finance/views/finance_view.dart
@@ -21,11 +21,14 @@ class _FinanceViewState extends State<FinanceView> {
   }
 
   Future<void> fetchNews() async {
-    final url = Uri.parse(
-      "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=c0a4ffdc12da4de18e879c1ccbcf649e",
-    );
+  final url = Uri.parse(
+    "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=c0a4ffdc12da4de18e879c1ccbcf649e",
+  );
 
+  try {
     final response = await http.get(url);
+
+    if (!mounted) return; // ✅ PENTING
 
     if (response.statusCode == 200) {
       final data = json.decode(response.body);
@@ -37,9 +40,15 @@ class _FinanceViewState extends State<FinanceView> {
       setState(() {
         isLoading = false;
       });
-      throw Exception("Gagal mengambil berita");
     }
+  } catch (e) {
+    if (!mounted) return;
+    setState(() {
+      isLoading = false;
+    });
   }
+}
+
 
   @override
   Widget build(BuildContext context) {
@@ -52,17 +61,26 @@ class _FinanceViewState extends State<FinanceView> {
             "EcoCampus",
             style: TextStyle(
               fontFamily: "poppins",
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
+              fontSize: 26,
+              fontWeight: FontWeight.w700,
+              letterSpacing: 0.5,
             ),
           ),
           const SizedBox(height: 20),
 
-          const Text(
-            "Berita Finance",
-            style: TextStyle(
-              fontSize: 22,
-              fontWeight: FontWeight.bold,
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 120, vertical: 10),
+            decoration: BoxDecoration(
+              color: Color.fromARGB(255, 64, 137, 186), // warna background
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: const Text(
+              "Berita Finance",
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w600,
+                color: Colors.black87,
+              ),
             ),
           ),
           const SizedBox(height: 20),
@@ -78,28 +96,30 @@ class _FinanceViewState extends State<FinanceView> {
                     crossAxisCount: 2,
                     crossAxisSpacing: 16,
                     mainAxisSpacing: 16,
-                    childAspectRatio: 0.85,
+                    childAspectRatio: 0.75,
                   ),
                   itemBuilder: (context, index) {
                     final article = articles[index];
 
-                    return GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (_) => DetailNewsView(
-                              url: article['url'],
-                              title: article['title'] ?? "Detail Berita",
-                            ),
+                    return InkWell(
+                    borderRadius: BorderRadius.circular(16),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (_) => DetailNewsView(
+                            url: article['url'],
+                            title: article['title'] ?? "Detail Berita",
                           ),
-                        );
-                      },
-                      child: financeCard(
-                        title: article['title'] ?? "No Title",
-                        imageUrl: article['urlToImage'],
-                      ),
-                    );
+                        ),
+                      );
+                    },
+                    child: financeCard(
+                      title: article['title'] ?? "No Title",
+                      imageUrl: article['urlToImage'],
+                    ),
+                  );
+
                   },
 
                 ),
@@ -107,61 +127,75 @@ class _FinanceViewState extends State<FinanceView> {
       ),
     );
   }
-
-  Widget financeCard({
-    required String title,
-    String? imageUrl,
-  }) {
-    return Container(
-      decoration: BoxDecoration(
-        border: Border.all(color: Colors.black),
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(16),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withOpacity(0.15),
-            blurRadius: 6,
-            offset: const Offset(0, 3),
-          )
-        ],
-      ),
-      child: Column(
-        children: [
-          ClipRRect(
-            borderRadius: const BorderRadius.vertical(top: Radius.circular(16)),
-            child: imageUrl != null
-                ? Image.network(
-                    imageUrl,
-                    height: 100,
-                    width: double.infinity,
-                    fit: BoxFit.cover,
-                  )
-                : Container(
-                    height: 100,
-                    color: Colors.grey[300],
-                    child: const Icon(Icons.image_not_supported),
-                  ),
+Widget financeCard({
+  required String title,
+  String? imageUrl,
+}) {
+  return Container(
+    decoration: BoxDecoration(
+      color: const Color.fromARGB(255, 255, 255, 255),
+      borderRadius: BorderRadius.circular(16),
+      boxShadow: [
+        BoxShadow(
+          color: Colors.black.withOpacity(0.08),
+          blurRadius: 10,
+          offset: const Offset(0, 6),
+        )
+      ],
+    ),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        // IMAGE
+        ClipRRect(
+          borderRadius: const BorderRadius.vertical(
+            top: Radius.circular(16),
           ),
-          Container(
-            width: double.infinity,
+          child: imageUrl != null
+              ? Image.network(
+                  imageUrl,
+                  height: 130,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                )
+              : Container(
+                  height: 120,
+                  color: Colors.grey[200],
+                  child: const Center(
+                    child: Icon(
+                      Icons.image_outlined,
+                      size: 40,
+                      color: Colors.grey,
+                    ),
+                  ),
+                ),
+        ),
+
+        // CONTENT
+        Expanded(
+          child: Container(
             padding: const EdgeInsets.all(12),
             decoration: const BoxDecoration(
-              color: Color(0xffCDEEFF),
-              borderRadius:
-                  BorderRadius.vertical(bottom: Radius.circular(16)),
+              color: Color.fromARGB(255, 64, 137, 186),
+              borderRadius: BorderRadius.vertical(
+                bottom: Radius.circular(16),
+              ),
             ),
             child: Text(
               title,
-              maxLines: 2,
+              maxLines: 3,
               overflow: TextOverflow.ellipsis,
               style: const TextStyle(
                 fontSize: 14,
                 fontWeight: FontWeight.w600,
+                height: 1.3,
+                color: Colors.black87,
               ),
             ),
           ),
-        ],
-      ),
-    );
-  }
+        ),
+      ],
+    ),
+  );
+}
 }
